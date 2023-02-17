a7aa34e62243731799cc281c5c512b33c4246f7e
Emmanuel <famakinwa486@gmail.com>
2023-02-16 17:40:58 -0800

    Index 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar.
+ * Retur: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
