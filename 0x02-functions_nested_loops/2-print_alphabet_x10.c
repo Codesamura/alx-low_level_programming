aa08166387ad7e5c44a6891cb85dfba744ea30f3
Emmanuel <famakinwa486@gmail.com>
2023-02-21 15:56:52 -0800

    Index 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - A function that prints the alphabets 10 times
+ *
+ * Return: x10 a-z
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, bo;
+
+	bo = 0;
+
+	while (bo < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		bo++;
+		_putchar('\n');
+	}
+}
