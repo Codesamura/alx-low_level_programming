65a2a370e6465a694d70b8be142ca7a1dff6f482
Emmanuel <famakinwa486@gmail.com>
2023-02-21 18:26:18 -0800

    Index 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers and prints the result
+ * @prime: first number
+ * @final: second number
+ * Return: prime + final
+ */
+int add(int prime, int final)
+{
+	return (prime + final);
+}
