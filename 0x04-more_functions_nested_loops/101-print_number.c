713909c17f06ecaaa09a800197c6f50f0392723a
Emmanuel <famakinwa486@gmail.com>
2023-02-23 12:15:38 -0800

    Index 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,13 +15,10 @@ void print_number(int n)
 	{
 		n *= -1;
 		k = n;
-		_putchar('-')
+		_putchar('-');
 	}
 	k /= 10;
-
 	if (k != 0)
-	{
 	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
-	}
 }
