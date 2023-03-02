88167fe40b32e08e28be9302717c9037b1b73333
Emmanuel <famakinwa486@gmail.com>
2023-03-02 06:09:10 -0800

    Index 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcat - This is a function strcat
+ * @dest: The first parameter
+ * @src: The second parameter
+ * Return: A string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '0' i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
