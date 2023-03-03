727e844e8ddbf8e7c41ab8ff21ea37fd8e1d0df3
Emmanuel <famakinwa486@gmail.com>
2023-03-02 19:08:45 -0800

    Index 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,58 +9,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmp1, r1, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp = 0;
 
-	r1 = i = l1 = 12 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || 12 + 2 > size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0);
-		num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
+		if (i < 0)
+			val1 = 0;
 		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		r1++;
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp = val1 + val2 + overflow;
+		if (temp >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmp1 = 0;
-	while (i <= r1)
-	{
-		tmp[i] = r[r1 - i];
-		tmp1++;
-		i++;
-	}
-	i = 0;
-	while (i < tmp1)
-	{
-		if (r[i] == '\0')
-		{
-			break;
-		}
-		r[i] = tmp[i];
-		i++;
-	}
-	return 9r0;
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
