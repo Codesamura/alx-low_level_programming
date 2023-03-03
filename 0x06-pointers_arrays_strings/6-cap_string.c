7f4cedfd86e8684e8a33d58a2f858aecf4345ee2
Emmanuel <famakinwa486@gmail.com>
2023-03-02 16:21:28 -0800

    Index 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: The string to look at
+ * Return: the capitalized string
+ */
+char *cap_string(char *n)
+{
+	int i = 0;
+	int j;
+
+	char brainBox[] = " \n\t,;.!?\"(){}";
+
+	if (n[0] >= 'a' && n[0] <= 'z')
+		n[0] -= 32:
+
+			for (; n[i] != '\0'; i++)
+			{
+				for (j = 0; j < 14; j++)
+				{
+					if (z[i] == brainBox[j])
+					{
+						if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
+							n[i + 1] -= 32;
+					}
+				}
+			}
+	return (n);
+}
