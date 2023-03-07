4cac4771440925391a3f9980cdf9b53159174358
Emmanuel <famakinwa486@gmail.com>
2023-03-07 10:42:13 -0800

    Index 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion _ Returns the value of x raised to the power of y
+ * _pow_recursion - Returns the value of x raised to the power of y
  * @x: The value to raised to a power
  * @y: The value of the power
  * Return: The value multiplied y times
