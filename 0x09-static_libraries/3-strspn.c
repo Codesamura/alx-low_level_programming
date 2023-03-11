c8b39b2b71688b4ee869ea0ec714b9436b58ab8b
Emmanuel <famakinwa486@gmail.com>
2023-03-10 20:00:44 -0800

    Index 10

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int letter;
+	int mat = 0;
+
+	while (s[mat])
+	{
+		for (letter = 0; accept[letter]; letter++)
+		{
+			if (accept[letter] == s[mat])
+			{
+				break;
+			}
+		}
+		if (!accept[letter])
+		{
+			break;
+		}
+		mat++;
+	}
+	return (mat);
+}
