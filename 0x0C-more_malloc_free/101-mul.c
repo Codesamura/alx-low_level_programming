f2d3cdb030231407ca4442f8524d506b7352de3c
Emmanuel <famakinwa486@gmail.com>
2023-03-17 10:05:27 -0700

    Index 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include "main.h"
-int main()
+
+/**
+ * main - .....
+ * num1: First number
+ * num2: Second number
+ * Return: Nothing
+ */
+int main(void)
 {
-	int num1,num2,product;
+	int num1, num2, mul;
+
 	printf("Enter two numbers:");
-	scanf("%d %d",&num1,&num2);
-	product=num1*num2;
-	printf("Product of two numbers: %d",product);
-	return  0;
+	scanf("%d %d", &num1, &num2);
+	product = num1 * num2;
+	printf("Product of two numbers: %d", product);
+	return  (0);
 }
