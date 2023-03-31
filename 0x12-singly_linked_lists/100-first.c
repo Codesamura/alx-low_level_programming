3c87f778d60eb17dfc39400db335e5ecce7fe562
Codesamura <famakinwa486@gmail.com>
2023-03-30 17:10:54 -0700

    Index 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-void first(void) _attribute_ ((constructor));
-
 /**
- * first - Prints a sentence before the main
- * Return: function is executed
+ * bmain - function executed before main
+ * Return: no return.
  */
-void first(void)
+
+void __attribute__ ((constructor)) bmain()
 {
-	printf("you're beat! and yet, you must allow.\n");
-	printf("I bore my houde upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
