240e153e58631a99f3c5910159d6d3b14a9ae969
Codesamura <famakinwa486@gmail.com>
2023-04-04 04:53:21 -0700

    Index 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listit_t *tmp, *copy = *head;
+	listint_t *tmp, *copy = *head;
 	unsigned int node;
 
 	if (copy == NULL)
