2f00b661c53e39d92d4c5c198f99d81dfd476291
Codesamura <famakinwa486@gmail.com>
2023-04-04 02:48:23 -0700

    Index 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: NULL if the function fails,else the address of the new element.
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
