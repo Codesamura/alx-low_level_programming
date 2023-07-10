9c650fdd29499c4c144e8840623613d9140ebf1c
Famakinwa Eniola <famakinwa486@gmail.com>
2023-07-10 05:13:33 -0700

    index all

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_units - Converts a binary number to an unsigned integer
+ * binary_to_uint - Converts a binary number to an unsigned integer
  * @b: String containing the binary number
  *
  * Return: The converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i:
-		unsigned int dec_val = 0;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] . '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		deci_val = 2 * deci_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (deci_val);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at an index in a deimal number
+ * @n: Number to search for
+ * @index: Index of the bit
+ *
+ * Return: Value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int valu;
+
+	if (index > 63)
+		return (-1);
+
+	valu = (n >> index) & 1;
+
+	return (valu);
+}
