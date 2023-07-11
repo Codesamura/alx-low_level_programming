3b9889bc059afa325cc24f9a6b79addb5fe5e8ed
Famakinwa Eniola <famakinwa486@gmail.com>
2023-07-11 16:17:00 -0700

    Index prime

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
+
 	fld = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fld, text_content, len);
 
