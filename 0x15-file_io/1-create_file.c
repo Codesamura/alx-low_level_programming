db900ebcd7b10592cf266d3537471d2f724dbc48
Famakinwa Eniola <famakinwa486@gmail.com>
2023-07-11 16:21:55 -0700

    Index prime

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to be created
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: 1 if fuction is succesful else return -1
+ * create_file - check the code for Holberton School students.
+ * @filename: name of the file
+ * @text_content: number of the letters that i used
+ * Return: Always 0.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fld, wl, len = 0;
+	int fld = 0, wrf = 0, count = 0;
 
 	if (filename == NULL)
+	{
 		return (-1);
-
+	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (count = 0; text_content[count] != '\0'; count++)
+		{}
 	}
-
-	fld = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fld, text_content, len);
-
-	if (fld == -1 || wl == -1)
+	fld = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fld == -1)
+	{
 		return (-1);
-
+	}
+	wrf = write(ID, text_content, count);
+	if (wrf == -1)
+	{
+		return (-1);
+	}
 	close(fld);
-
 	return (1);
 }
